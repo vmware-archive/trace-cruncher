89b07713b28652a53f70cb0abf20ed5d07fbb3ff
Yordan Karadzhov (VMware) <y.karadz@gmail.com>
2019-10-25 21:32:10 +0300

    Improve the sched_wakeup.py example script.
    Signed-off-by: Yordan Karadzhov (VMware) <y.karadz@gmail.com>

--- examples/sched_wakeup.py
+++ examples/sched_wakeup.py
@@ -9,6 +9,7 @@ Copyright 2019 VMware Inc, Yordan Karadzhov <ykaradzhov@vmware.com>
 import os
 import sys
 import json
+import pprint as pr
 
 import matplotlib.pyplot as plt
 import scipy.stats as st
@@ -19,20 +20,32 @@ from ksharksetup import setup
 setup()
 
 import ksharkpy as ks
+# Get the name of the user program.
+if len(sys.argv) >= 2:
+    fname = str(sys.argv[1])
+else:
+    fname = input('choose a trace file: ')
 
-fname = str(sys.argv[1])
+status = ks.open_file(fname)
+if not status:
+    print ("Failed to open file ", fname)
+    sys.exit()
 
-ks.open_file(fname)
+ks.register_plugin('reg_pid')
 
 # We do not need the Process Ids of the records.
 # Do not load the "pid" data.
 data = ks.load_data(pid_data=False)
+tasks = ks.get_tasks()
 
 # Get the name of the user program.
-prog_name = str(sys.argv[2])
+if len(sys.argv) >= 3:
+    prog_name = str(sys.argv[2])
+else:
+    pr.pprint(tasks)
+    prog_name = input('choose a task: ')
 
-tasks = ks.get_tasks()
-task_pid = tasks[prog_name]
+task_pid = tasks[prog_name][0]
 
 # Get the Event Ids of the sched_switch and sched_waking events.
 ss_eid = ks.event_id('sched', 'sched_switch')
@@ -54,9 +67,27 @@ while i > 0:
         if next_pid == task_pid:
             time_ss = data['time'][i]
             index_ss = i
+            cpu_ss = data['cpu'][i]
 
             while i > 0:
                 i = i - 1
+
+                if data['event'][i] < 0 and cpu_ss == data['cpu'][i]:
+			# Ring buffer overflow. Ignore this case and continue.
+                        break
+
+                if data['event'][i] == ss_eid:
+                    next_pid = ks.read_event_field(offset=data['offset'][i],
+                                       event_id=ss_eid,
+                                       field='next_pid')
+                    if next_pid == task_pid:
+                        # Second sched_switch for the same task. ?
+                        time_ss = data['time'][i]
+                        index_ss = i
+                        cpu_ss = data['cpu'][i]
+
+                    continue
+
                 if (data['event'][i] == w_eid):
                     waking_pid = ks.read_event_field(offset=data['offset'][i],
                                                      event_id=w_eid,
@@ -84,8 +115,8 @@ rect = fig.patch
 rect.set_facecolor('white')
 
 ax.set_xlabel('latency [$\mu$s]')
-plt.yscale('log')
-ax.hist(dt, bins=(200), histtype='step')
+#plt.yscale('log')
+ax.hist(dt, bins=(100), histtype='step')
 plt.show()
 
 sname = 'sched.json'
@@ -94,9 +125,10 @@ ks.new_session(fname, sname)
 with open(sname, 'r+') as s:
     session = json.load(s)
     session['TaskPlots'] = [task_pid]
-    session['CPUPlots'] = [
-        int(data['cpu'][i_sw_max]),
-        int(data['cpu'][i_ss_max])]
+    session['CPUPlots'] = [int(data['cpu'][i_sw_max])]
+
+    if data['cpu'][i_ss_max] != data['cpu'][i_sw_max]:
+        session['CPUPlots'].append(int(data['cpu'][i_ss_max]))
 
     delta = data['time'][i_ss_max] - data['time'][i_sw_max]
     tmin = int(data['time'][i_sw_max] - delta)
