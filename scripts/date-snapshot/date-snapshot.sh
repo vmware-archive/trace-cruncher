d8dc18080c431abce203073aa1fc80062ab2ce58
June Knauth <june.knauth@gmail.com>
2022-06-28 17:17:36 -0700

    trace-cruncher: Add more flags and workdir tracking to date-snapshot
    This commit further extends the feature set of date-snapshot and helps
    ensure it will behave as expected in all situations.
    
    Added logging levels so that users can debug the behavior of the script.
    By default, only critical errors are printed.
    
    Added a --latest flag to bypass the specified date and get the latest version
    of the repository. This will be leveraged in GitHub actions to check for
    regressions or breaking changes automatically.
    
    Added workdir tracking to the script. When the -f flag is passed, the workdir
    is the parent dir of the provided file. When -i is passed, the workdir is the
    current working directory.
    
    Workdir tracking ensures commands like -d will always execute in the same
    directory as the repos were created in, as long as the input file is the same.
    
    Signed-off-by: June Knauth (VMware) <june.knauth@gmail.com>

--- scripts/date-snapshot/date-snapshot.sh
+++ scripts/date-snapshot/date-snapshot.sh
@@ -7,40 +7,67 @@
 # Takes inputs specified via command line or in a file
 
 package=date-snapshot
-clean="false" # clean and download define the script's behavior
-download="true"
-# If clean is set, we delete existing repos. If not, we leave them untouched (no checkout).
-# If download is set, we download and checkout the repos specified if they don't exist.
+clean="false" # Delete existing repos
+download="true" # Download and checkout the repos specified if they don't exist
+verbose="false" # Print non-critical debug info and warnings
+latest="false" # Ignore the specified date and get the latest version
 
+# Get absolute path of the folder we're working in, derived from the file path for -f
+# If that command fails, abort
+get_absolute_folder(){
+  name=$(dirname "$1") # directory which contains our input
+  dir_valid=$(if [[ -d "${name}" ]]; then echo "true"; fi) # is this directory valid?
+  cd "${name}" # attempt to cd to that directory
+  if [[ $dir_valid = "true" && $? = "0" ]] ; then # if the directory is valid and cd succeeds
+    echo $(pwd -P)
+  else
+    log_critical "*** Error: Could not get path info for input file."
+  fi
+}
+
+# log_critical always prints for critical information
+log_critical(){
+echo "$1"
+}
+
+# log_verbose only prints when the -v flag is set
+log_verbose(){
+if [[ $verbose = "true" ]] ; then
+echo "$1"
+fi
+}
+
+# the main function to download and checkout the specified repositories
 download_checkout(){
   IN="${1}"
   IFS=';' read -ra ADDR <<< "$IN"
 
   if [[ $clean = "true" ]] ; then # delete the repos if requested
-    if [[ ! -d "${ADDR[0]}" ]] ; then
-      echo "*** Warning: Directory ${ADDR[0]} does not exist, nothing to delete"
+    if [[ ! -d "${workdir}/${ADDR[0]}" ]] ; then
+      log_verbose "*** Warning: Directory ${workdir}/${ADDR[0]} does not exist, nothing to delete"
     else
-      echo "Removing ${ADDR[0]}"
-      rm -rf "./${ADDR[0]}"
+      log_verbose "Removing ${workdir}/${ADDR[0]}"
+      rm -rf "${workdir}/${ADDR[0]}"
     fi
   fi
 
   # if download is true and directory doesn't exist
-  if [[ $download = "true" && ! -d "${ADDR[0]}" ]] ; then
-    git clone -b "${ADDR[2]}" "${ADDR[1]}" "${ADDR[0]}"
-    cd "${ADDR[0]}"
-
-    last_date=`date -d "${ADDR[3]}" +%s` # convert given date to unix timestamp
-    # we could simply give the param to git but it understands fewer formats than date
-
-    # this checks out the first entry in the list of commit hashes which occur before our date
-    git checkout `git log --date=unix --before="${last_date}" --pretty=format:"%H" | head -n 1`
-
-    cd ..
+  if [[ $download = "true" && ! -d "${workdir}/${ADDR[0]}" ]] ; then
+    git clone -b "${ADDR[2]}" "${ADDR[1]}" "${workdir}/${ADDR[0]}"
+    if [[ $latest = "false" ]] ; then # If latest flag is set, leave repo as-is
+      cd "${workdir}/${ADDR[0]}"
+      last_date=`date -d "${ADDR[3]}" +%s` # convert given date to unix timestamp
+      # we could simply give the param to git but it understands fewer formats than date
+      # this checks out the first entry in the list of commit hashes which occur before our date
+      git checkout `git log --date=unix --before="${last_date}" --pretty=format:"%H" | head -n 1`
+      cd "${workdir}"
+    else
+      log_verbose "Latest flag set, skipping checkout for ${ADDR[0]}"
+    fi
   elif [[ $download = "false" ]] ; then
-    echo "Download flag not set, skipping ${ADDR[0]}"
-  elif [[ -d "${ADDR[0]}" ]] ; then
-    echo "Directory exists, skipping ${ADDR[0]}"
+    log_verbose "Download flag not set, skipping ${ADDR[0]}"
+  elif [[ -d "${workdir}/${ADDR[0]}" ]] ; then
+    log_verbose "Directory exists, skipping ${ADDR[0]}"
   fi
 
   unset IFS
@@ -59,6 +86,8 @@ while test $# -gt 0; do
       echo "-h, --help                show brief help"
       echo "-c, --clean               delete the directories specified in the input and continue"
       echo "-d, --delete              delete the directories specified in the input and exit"
+      echo "-v, --verbose             print debugging information to the console"
+      echo "-l, --latest              ignore specified date and checkout the latest versions"
       echo "-i, --input REPOS         specify a space-sep'd list of repos to download"
       echo "-f, --file FILE           specify an input file, one repo per line"
       exit 0
@@ -73,8 +102,17 @@ while test $# -gt 0; do
       clean="true"
       download="false"
       ;;
+    -v|--verbose)
+      shift
+      verbose="true"
+      ;;
+    -l|--latest)
+      shift
+      latest="true"
+      ;;
     -i|--input)
       shift
+      workdir="${pwd}"
       IFS=" "
       while read repo; do
         download_checkout $repo
@@ -84,13 +122,14 @@ while test $# -gt 0; do
       ;;
     -f|--file)
       shift
+      workdir="$(get_absolute_folder "$1")"
       while read line; do
         download_checkout $line
       done <$1
       break
       ;;
     *)
-      echo "Usage: ${package} [-c|--clean] [-d|delete] [-i|--input] [-f|--file] <input>"
+      echo "Usage: ${package} [-c] [-d] [-v] [-l] [-i|--input] [-f|--file] <input>"
       echo "${package} --help for more"
       break
       ;;
