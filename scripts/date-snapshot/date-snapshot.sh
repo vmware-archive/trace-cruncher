0f59976ab5583f6aea0c300420ef606933e91a06
June <cknauth119@gmail.com>
2022-06-29 19:59:15 -0700

    trace-cruncher: Fix CI issue and add reproducibility tools (#3)
    * trace-cruncher: Fix CI issue and add reproducibility tools
    
    This commit adds a scripts/ directory and the date-snapshot.sh script.
    date-snapshot automates the process of checking out the last commit
    in a repository before a specific date.
    
    We leverage this tool in the GitHub actions CI to fix the ongoing build
    error by locking our build dependencies to a specific date- currently
    June 1, 2022. This date can be advanced when the breaking change is
    found and fixed in the dependencies.
    
    This commit is the beginning of work to make the trace-cruncher build
    fully reproducible for containerization.
    
    Signed-off-by: June Knauth (VMware) <june.knauth@gmail.com>
    Reviewed-by: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>
    
    * trace-cruncher: Implement PR feedback
    
    This commit adds arguments to the date-snapshot script to enable deleting existing directories and either exiting or proceeding from there. This functionality is used in `make clean` to remove build artifacts. It also eases iterative testing when using the script.
    
    In addition, the git source for kernel-shark has been changed to the mainline kernel.org repo because the old remote was used for testing and was not intended for stable release.
    
    Finally, whitespace was cleaned up and invisible characters removed for easier readability.
    
    Signed-off-by: June Knauth (VMware) <june.knauth@gmail.com>
    Reviewed-by: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>
    
    * trace-cruncher: Refactor and improve date-snapshot
    
    This commit improves the behavior of date-snapshot in unexpected conditions and improves readability.
    
    Explicit conditionals added as they are best practice to ensure consistent behavior.
    In addition, quotes added around all inputs to the script to prevent multiple arguments from being passed to commands.
    Finally, added a warning when a directory that is about to be deleted does not exist so that users are warned in unexpected situations.
    
    Signed-off-by: June Knauth (VMware) <june.knauth@gmail.com>
    Reviewed-by: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>
    
    * trace-cruncher: Add more flags and workdir tracking to date-snapshot
    
    This commit further extends the feature set of date-snapshot and helps
    ensure it will behave as expected in all situations.
    
    Added logging levels so that users can debug the behavior of the script.
    By default, only critical errors are printed.
    
    Added a --latest flag to bypass the specified date and get the latest version
    of the repository. This will be leveraged in GitHub actions to check for
    regressions or breaking changes automatically.
    
    Added workdir tracking to the script. When the -f flag is passed, the workdir
    is the parent dir of the provided file. When -i is passed, the workdir is the
    current working directory.
    
    Workdir tracking ensures commands like -d will always execute in the same
    directory as the repos were created in, as long as the input file is the same.
    
    Signed-off-by: June Knauth (VMware) <june.knauth@gmail.com>
    Reviewed-by: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>

---
+++ scripts/date-snapshot/date-snapshot.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: LGPL-2.1
+# Copyright (C) 2022, VMware Inc, June Knauth <june.knauth@gmail.com>
+#
+# Script to download one or several git repos at the last commit before a specified date
+# Takes inputs specified via command line or in a file
+
+package=date-snapshot
+clean="false" # Delete existing repos
+download="true" # Download and checkout the repos specified if they don't exist
+verbose="false" # Print non-critical debug info and warnings
+latest="false" # Ignore the specified date and get the latest version
+
+# Get absolute path of the folder we're working in, derived from the file path for -f
+# If that command fails, abort
+get_absolute_folder(){
+  name=$(dirname "$1") # directory which contains our input
+  dir_valid=$(if [[ -d "${name}" ]]; then echo "true"; fi) # is this directory valid?
+  cd "${name}" # attempt to cd to that directory
+  if [[ $dir_valid = "true" && $? = "0" ]] ; then # if the directory is valid and cd succeeds
+    echo $(pwd -P)
+  else
+    log_critical "*** Error: Could not get path info for input file."
+  fi
+}
+
+# log_critical always prints for critical information
+log_critical(){
+echo "$1"
+}
+
+# log_verbose only prints when the -v flag is set
+log_verbose(){
+if [[ $verbose = "true" ]] ; then
+echo "$1"
+fi
+}
+
+# the main function to download and checkout the specified repositories
+download_checkout(){
+  IN="${1}"
+  IFS=';' read -ra ADDR <<< "$IN"
+
+  if [[ $clean = "true" ]] ; then # delete the repos if requested
+    if [[ ! -d "${workdir}/${ADDR[0]}" ]] ; then
+      log_verbose "*** Warning: Directory ${workdir}/${ADDR[0]} does not exist, nothing to delete"
+    else
+      log_verbose "Removing ${workdir}/${ADDR[0]}"
+      rm -rf "${workdir}/${ADDR[0]}"
+    fi
+  fi
+
+  # if download is true and directory doesn't exist
+  if [[ $download = "true" && ! -d "${workdir}/${ADDR[0]}" ]] ; then
+    git clone -b "${ADDR[2]}" "${ADDR[1]}" "${workdir}/${ADDR[0]}"
+    if [[ $latest = "false" ]] ; then # If latest flag is set, leave repo as-is
+      cd "${workdir}/${ADDR[0]}"
+      last_date=`date -d "${ADDR[3]}" +%s` # convert given date to unix timestamp
+      # we could simply give the param to git but it understands fewer formats than date
+      # this checks out the first entry in the list of commit hashes which occur before our date
+      git checkout `git log --date=unix --before="${last_date}" --pretty=format:"%H" | head -n 1`
+      cd "${workdir}"
+    else
+      log_verbose "Latest flag set, skipping checkout for ${ADDR[0]}"
+    fi
+  elif [[ $download = "false" ]] ; then
+    log_verbose "Download flag not set, skipping ${ADDR[0]}"
+  elif [[ -d "${workdir}/${ADDR[0]}" ]] ; then
+    log_verbose "Directory exists, skipping ${ADDR[0]}"
+  fi
+
+  unset IFS
+}
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "$package - Download a git repo and checkout the last commit before a given date"
+      echo "Takes CLI and file arguments with format '<repo name>;<repo url>;<branch>;<date>'"
+      echo "Dates can be provided in any format the unix 'date' command understands."
+      echo " "
+      echo "$package [options] [arguments]"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "-c, --clean               delete the directories specified in the input and continue"
+      echo "-d, --delete              delete the directories specified in the input and exit"
+      echo "-v, --verbose             print debugging information to the console"
+      echo "-l, --latest              ignore specified date and checkout the latest versions"
+      echo "-i, --input REPOS         specify a space-sep'd list of repos to download"
+      echo "-f, --file FILE           specify an input file, one repo per line"
+      exit 0
+      ;;
+    -c|--clean)
+      shift
+      clean="true"
+      download="true"
+      ;;
+    -d|--delete)
+      shift
+      clean="true"
+      download="false"
+      ;;
+    -v|--verbose)
+      shift
+      verbose="true"
+      ;;
+    -l|--latest)
+      shift
+      latest="true"
+      ;;
+    -i|--input)
+      shift
+      workdir="${pwd}"
+      IFS=" "
+      while read repo; do
+        download_checkout $repo
+      done <<< $1
+      unset IFS
+      break
+      ;;
+    -f|--file)
+      shift
+      workdir="$(get_absolute_folder "$1")"
+      while read line; do
+        download_checkout $line
+      done <$1
+      break
+      ;;
+    *)
+      echo "Usage: ${package} [-c] [-d] [-v] [-l] [-i|--input] [-f|--file] <input>"
+      echo "${package} --help for more"
+      break
+      ;;
+  esac
+done
+
